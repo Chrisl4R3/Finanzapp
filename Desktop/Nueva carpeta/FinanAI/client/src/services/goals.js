609fb5e955faf23f2dc739fa382ac63708dd5522
Chrisl4R3 <densartunal12345@gmail.com>
2025-06-02 15:04:24 -0400

    Update goals.js

--- Desktop/Nueva carpeta/FinanAI/client/src/services/goals.js
+++ Desktop/Nueva carpeta/FinanAI/client/src/services/goals.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://backend-production-cf437.up.railway.app/api';
 
 // Obtener todas las metas
 export const getAllGoals = async () => {
